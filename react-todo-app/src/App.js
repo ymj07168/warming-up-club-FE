27a756e6189e429d70b54950426f514c707cee6f
이효원 <ymj07168@gmail.com>
2024-07-10 21:13:31 +0900

    feat: 전체 할 일 목록 제거

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -7,6 +7,11 @@ export default function App() {
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
 
+  // 전체 할 일 제거
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
   // 할 일 제거
   const handleClick = useCallback(
     (id) => {
@@ -36,6 +41,7 @@ export default function App() {
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
         <List
           todoData={todoData}
