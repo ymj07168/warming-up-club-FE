267f770b569c6ab2f5835a4d12b5911f783e5a77
이효원 <ymj07168@gmail.com>
2024-07-10 20:38:21 +0900

    feat: 리스트아이템 컴포넌트 분리

--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,24 +1,8 @@
 import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import ListItem from "./ListItem";
 
 export default function List({ todoData, setTodoData }) {
-  // 체크박스 핸들러
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
-
-  // 할 일 제거
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    setTodoData(newTodoData);
-  };
-
   const handleEnd = (result) => {
     console.log("result", result);
 
@@ -47,39 +31,16 @@ export default function List({ todoData, setTodoData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
+                    <ListItem
                       key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
-                    >
-                      <div className="items-center">
-                        <input
-                          type="checkbox"
-                          defaultChecked={false}
-                          checked={data.completed}
-                          onChange={() => handleCompleteChange(data.id)}
-                        />{" "}
-                        <span
-                          className={
-                            data.completed ? "line-through" : undefined
-                          }
-                        >
-                          {data.title}
-                        </span>
-                      </div>
-                      <div>
-                        <button
-                          className="px-4 py-2"
-                          onClick={() => handleClick(data.id)}
-                        >
-                          x
-                        </button>
-                      </div>
-                    </div>
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
                   )}
                 </Draggable>
               ))}
---
+++ react-todo-app/src/components/ListItem.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function ListItem({
+  key,
+  id,
+  title,
+  completed,
+  todoData,
+  setTodoData,
+  provided,
+  snapshot,
+}) {
+  // 체크박스 핸들러
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTodoData(newTodoData);
+  };
+
+  // 할 일 제거
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    setTodoData(newTodoData);
+  };
+
+  return (
+    <div
+      key={key}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+      className={`${
+        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+    >
+      <div className="items-center">
+        <input
+          type="checkbox"
+          defaultChecked={false}
+          checked={completed}
+          onChange={() => handleCompleteChange(id)}
+        />{" "}
+        <span className={completed ? "line-through" : undefined}>{title}</span>
+      </div>
+      <div>
+        <button className="px-4 py-2" onClick={() => handleClick(id)}>
+          x
+        </button>
+      </div>
+    </div>
+  );
+}
