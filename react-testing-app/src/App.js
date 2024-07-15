b90d86da97960aebcf25fe6121c7cbdbffdc2f59
이효원 <ymj07168@gmail.com>
2024-07-15 23:10:10 +0900

    feat: on/off 버튼 추가

--- react-testing-app/src/App.js
+++ react-testing-app/src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -11,15 +11,26 @@ function App() {
         <button
           data-testid="minus-button"
           onClick={() => setCount((prev) => prev - 1)}
+          disabled={disabled}
         >
           -
         </button>
         <button
           data-testid="plus-button"
           onClick={() => setCount((prev) => prev + 1)}
+          disabled={disabled}
         >
           +
         </button>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
