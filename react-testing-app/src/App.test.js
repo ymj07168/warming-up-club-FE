d44409bd4feed61a61a5e6163f68c337ef12af20
이효원 <ymj07168@gmail.com>
2024-07-15 22:49:38 +0900

    feat: 버튼 테스트 코드 추가

--- react-testing-app/src/App.test.js
+++ react-testing-app/src/App.test.js
@@ -6,3 +6,16 @@ test("the counter starts at 0", () => {
   const counterElement = screen.getByTestId("counter");
   expect(counterElement).toHaveTextContent(0);
 });
+
+test("minus button has correct text", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+});
